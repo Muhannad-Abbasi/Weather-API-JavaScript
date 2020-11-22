936544b0165700648333b816fb6a1b46991ac05f
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2020-11-22 21:55:03 +0100

    Final Touches

--- Kunskapskontroll 2/JavaScript/app.js
+++ Kunskapskontroll 2/JavaScript/app.js
@@ -1,8 +1,9 @@
-// Skapa variabel och välj body för att sätta en huvudbakgrundsbild till webbplatsen
-let body = document.querySelector('body');
-body.style.backgroundImage = 'url(Img/main-background.jpg)';
-body.style.backgroundSize = 'cover';
-body.style.backgroundRepeat = 'no-repeat';
+// Skapa variabel och välj containerB för att sätta en huvudbakgrundsbild till webbplatsen
+let containerB = document.querySelector('#container-b');
+containerB.style.backgroundImage = 'url(Img/main-background.jpg)';
+containerB.style.backgroundSize = 'cover';
+containerB.style.backgroundRepeat = 'no-repeat';
+containerB.style.height = '89vh'
 
 // Skapa variabel till min apiKey
 const apiKey = 'a58f83c4989c4227461d5a79c362a60b';
@@ -35,13 +36,13 @@ weatherForm.addEventListener('submit',
                 pWeatherDescription.innerHTML = `Description: ${data.weather[0].description}`;
 
                 let pTemp = document.querySelector('#pTemp');
-                pTemp.innerHTML = `Temperature är: ${data.main.temp} °C.`;
+                pTemp.innerHTML = `Temperature Is: ${data.main.temp} °C.`;
 
                 let pWind = document.querySelector('#pWind');
-                pWind.innerHTML = `Wind speed is: ${data.wind.speed} Km/h`;
+                pWind.innerHTML = `Wind Speed Is: ${data.wind.speed} Km/h`;
 
                 let pHumidity = document.querySelector('#pHumidity')
-                pHumidity.innerHTML = `Humidity is: ${data.main.humidity}`;
+                pHumidity.innerHTML = `Humidity Is: ${data.main.humidity}%`;
 
                 let img = document.querySelector('#wIcon')
                 let wIcon = data.weather[0].icon;
@@ -49,64 +50,90 @@ weatherForm.addEventListener('submit',
                 console.log(iconUrl)
                 img.src = iconUrl;
 
-
                 // Skapa en funktion som vi använder inuti det if-else statement för att sätta en kort fras och bakgrundsbild som kommer att förändras beror på temperaturdata vi får från API
                 function tempChange(){
                     if(data.main.temp <= -1){
-                        let body = document.querySelector('body');
-                        body.style.backgroundImage = 'url(Img/snow.jpg)';
-                        body.style.backgroundPosition = 'center center';
-                        body.style.backgroundSize = 'cover';
+                        let containerB = document.querySelector('#container-b');
+                        containerB.style.backgroundImage = 'url(Img/snow.jpg)';
+                        containerB.style.backgroundPosition = 'center center';
+                        containerB.style.backgroundSize = 'cover';
 
                         let tempCh = document.querySelector('#tempCh')
-                        tempCh.innerHTML = 'its a very cold day'
-                        tempCh.style.color = 'white';
-                        tempCh.style.backgroundColor = 'blue';
+                        tempCh.innerHTML = `It's a very cold day`;
+                        
+                        let uI = document.querySelector('.user-interface');
+                        uI.style.border = '3px solid blue';
+
+                        let uX = document.querySelector('.user-experience');
+                        uX.style.border = '3px solid blue';
 
                     }else if(data.main.temp == 0 || data.main.temp <= 17){
-                        let body = document.querySelector('body');
-                        body.style.backgroundImage = 'url(Img/rain.jpg)';
-                        body.style.backgroundPosition = 'center center';
-                        body.style.backgroundSize = 'cover';
+                        let containerB = document.querySelector('#container-b');
+                        containerB.style.backgroundImage = 'url(Img/rain.jpg)';
+                        containerB.style.backgroundPosition = 'center center';
+                        containerB.style.backgroundSize = 'cover';
 
                         let tempCh = document.querySelector('#tempCh')
-                        tempCh.innerHTML = 'its a cold day'
-                        tempCh.style.color = 'white';
-                        tempCh.style.backgroundColor = 'blue';
+                        tempCh.innerHTML = `It's a cold day`;
+                        
+                        let uI = document.querySelector('.user-interface');
+                        uI.style.border = '3px solid #02b3e4';
+
+                        let uX = document.querySelector('.user-experience');
+                        uX.style.border = '3px solid #02b3e4';
                         
                     }else if(data.main.temp >= 18 && data.main.temp <= 27){
-                        let body = document.querySelector('body');
-                        body.style.backgroundImage = 'url(Img/summer.jpg)';
-                        body.style.backgroundPosition = 'center center';
-                        body.style.backgroundSize = 'cover';
+                        let containerB = document.querySelector('#container-b');
+                        containerB.style.backgroundImage = 'url(Img/summer.jpg)';
+                        containerB.style.backgroundPosition = 'center center';
+                        containerB.style.backgroundSize = 'cover';
 
                         let tempCh = document.querySelector('#tempCh')
-                        tempCh.innerHTML = 'its a beautiful day'
-                        tempCh.style.color = 'white';
-                        tempCh.style.backgroundColor = 'green';
+                        tempCh.innerHTML = `It's a beautiful day`;
                         
+                        let uI = document.querySelector('.user-interface');
+                        uI.style.border = '3px solid green';
+
+                        let uX = document.querySelector('.user-experience');
+                        uX.style.border = '3px solid green';
+
                     }else if(data.main.temp >= 28){
-                        let body = document.querySelector('body');
-                        body.style.backgroundImage = 'url(Img/hot-summer.jpg)';
-                        body.style.backgroundPosition = 'center center';
-                        body.style.backgroundSize = 'cover';
-                        body.style.backgroundRepeat = 'no-repeat';
+                        let containerB = document.querySelector('#container-b');
+                        containerB.style.backgroundImage = 'url(Img/hot-summer.jpg)';
+                        containerB.style.backgroundPosition = 'center center';
+                        containerB.style.backgroundSize = 'cover';
+                        containerB.style.backgroundRepeat = 'no-repeat';
 
                         let tempCh = document.querySelector('#tempCh')
-                        tempCh.innerHTML = 'its a very hot day'
-                        tempCh.style.color = 'white';
-                        tempCh.style.backgroundColor = 'red';
+                        tempCh.innerHTML = `It's a very hot day`;
+
+                        let uI = document.querySelector('.user-interface');
+                        uI.style.border = '3px solid red';
+
+                        let uX = document.querySelector('.user-experience');
+                        uX.style.border = '3px solid red';
                     }
                 }
                 tempChange()
+
+                // Skapa en design function
+                function design(){
+                    let uX = document.querySelector('.user-experience');
+                    uX.style.width = '295px';
+                    uX.style.height = '17rem';
+                    uX.style.backgroundColor = ' rgb(199, 235, 255)';
+                    uX.style.borderRadius = '5px';
+                    uX.style.boxShadow = '2px 0px 8px rgb(204, 204, 204)';
+                    uX.style.padding = '25px 5px';
+                }
+                design()
             }
 
             // Använda catch metoden för att visa ett felmeddelande till användaren om de inte angav något i textformuläret eller om de anger fel stadsnamn
         ).catch(
             function (error){
-                let body = document.querySelector('form');
-                body.insertAdjacentHTML('afterend', `<p>Oops something went wrong! ${error}</p>`)
-                console.log(error)
+                let containerB = document.querySelector('form');
+                containerB.insertAdjacentHTML('afterend', `<p>Oops something went wrong! ${error}</p>`)
             }
         )
     }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kunskapskontroll-2-Muhannad-Abbasi
+
+A simple weather web page, where you will enter any city you want and then click on the search icon, and will automatically show you the weather status of this city
